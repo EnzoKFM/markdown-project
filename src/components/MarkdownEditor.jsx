229a956dc8a5e83084d02d35229a44fc20eaa802
Marevan Conti <marevanconti@gmail.com>
2025-12-03 15:05:03 +0100

    (+) Markdown - Added dependency "markdown-it" for preview - Using useState for text management in Editor - Added devDependency "tailwindcss/typography" for default style of markdown

--- src/components/MarkdownEditor.jsx
+++ src/components/MarkdownEditor.jsx
@@ -1,20 +1,25 @@
+import { useState } from "react";
 import MarkdownPreview from "./MarkdownPreview";
 
 export default function MarkdownEditor() {
+    const [text, setText] = useState("");
+
     return (
-        <div className="w-full h-screen bg-slate-950 text-slate-100 p-6">
+        <div className="w-full bg-slate-950 text-slate-100 p-6">
             <div className="max-w-6xl mx-auto h-full grid grid-cols-1 lg:grid-cols-2 gap-6">
 
                 <div className="flex flex-col h-full">
                     <h2 className="text-xl font-semibold mb-4">Markdown</h2>
 
                     <textarea
+                        value={text}
+                        onChange={(e) => setText(e.target.value)}
                         placeholder="# Markdown..."
                         className="flex-1 w-full bg-slate-900 border border-slate-700 rounded-xl p-4 text-slate-200 font-mono text-sm outline-none resize-none shadow-inner"
                     />
                 </div>
 
-                <MarkdownPreview />
+                <MarkdownPreview content={text} />
 
             </div>
         </div>
--- src/components/MarkdownPreview.jsx
+++ src/components/MarkdownPreview.jsx
@@ -1,10 +1,24 @@
+import MarkdownIt from "markdown-it";
+
+const md = new MarkdownIt({
+    html: true,
+    linkify: true,
+    typographer: true,
+    breaks: true
+});
+
 export default function MarkdownPreview({ content }) {
+
+    const rendered = md.render(content || "");
+
     return (
         <div className="flex flex-col h-full">
             <h2 className="text-xl font-semibold mb-4">Preview</h2>
 
-            <div className="flex-1 w-full bg-slate-900 border border-slate-700 rounded-xl p-4 text-slate-300">
-            </div>
+            <div
+                className="prose prose-slate prose-invert flex-1 w-full bg-slate-900 border border-slate-700 rounded-xl p-4 text-slate-300 overflow-auto max-w-none"
+                dangerouslySetInnerHTML={{ __html: rendered }}
+            />
         </div>
-    )
+    );
 }
